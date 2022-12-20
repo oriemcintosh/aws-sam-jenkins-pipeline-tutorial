41bc47d73907e03112479bdb6d3223600599b9a6
Orie McIntosh <orie.mcintosh@slalom.com>
2022-12-20 10:33:01 -0500

    updated app for pipeline testing

--- hello-world/app.js
+++ hello-world/app.js
@@ -21,7 +21,7 @@ exports.lambdaHandler = async (event, context) => {
     response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: "Hello World from ACG!!!!",
+        message: "Hello World from Cloud Enablement!",
         // location: ret.data.trim()
       }),
     };
