c2800700983eab4d1a3681231e039093f9ff1219
Orie McIntosh <orie.mcintosh@slalom.com>
2022-12-21 17:45:52 -0500

    updated deploy script

--- config/sam-deploy.sh
+++ config/sam-deploy.sh
@@ -4,9 +4,11 @@ samStatusCode=$?
 
 echo "...Deploying sam-app-recovery-plan-orchestration ..."
 sam deploy 
-if [ ${samStatusCode} -eq 2]; then 
-    echo "No updates required."
-else
+if [ ${samStatusCode} -eq 0]; then 
+    echo "Successfully created stack."
+elif [ ${samStatusCode} -eq 2]; then
+    echo "Successfully updated stack."
+else 
     response=$?
     exit ${response}
 fi
