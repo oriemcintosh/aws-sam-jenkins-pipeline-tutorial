02ff32ffab714784786f4f29be84e8073eb5ab14
Orie McIntosh <orie.mcintosh@slalom.com>
2022-12-22 09:58:47 -0500

    adding cfn-nag buildspec file

--- config/sam-deploy.sh
+++ config/sam-deploy.sh
@@ -2,12 +2,10 @@
 
 samStatusCode=$?
 
-echo "...Deploying sam-app-recovery-plan-orchestration ..."
-sam deploy 
+echo "...Deploying SAM App ..."
+sam deploy
 if [ ${samStatusCode} -eq 0 ]; then 
-    echo "Successfully created / updated stack."
-elif [ ${samStatusCode} -eq 2 ]; then
-    echo "No changes necessary."
+    echo "Done"
 else 
     response=$?
     exit ${response}
