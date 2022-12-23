66b39f839d7368e3dc90032b0e41f206cbb06502
Orie McIntosh <orie.mcintosh@slalom.com>
2022-12-23 12:25:53 -0500

    updated lambda response message

--- hello-world/app.js
+++ hello-world/app.js
@@ -21,7 +21,7 @@ exports.lambdaHandler = async (event, context) => {
     response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: "Hello World from Cloud Enablement @ Thu Dec 22 15:04:40 EST 2022",
+        message: "Hello World from Cloud Enablement @ Fri Dec 23 12:23:19 EST 2022",
         // location: ret.data.trim()
       }),
     };
