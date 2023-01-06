12da38387a6c69dc4691c38c978344016d5c3981
Orie McIntosh <orie.mcintosh@slalom.com>
2023-01-06 14:39:23 -0500

    Updated samconfig for a 'QA' config-env. Updated sam-deploy script to make use of EnvSuffix passed from SAM template. Added EnvSuffix to SAM template.

--- config/sam-deploy.sh
+++ config/sam-deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-samStatusCode=$?
+# command to gather parameter value from AWS and assign to $EnvSuffix variable
+EnvSuffix=$(aws cloudformation describe-stacks --stack-name recovery-plan-orchestration |jq '.Stacks[].Parameters | map(select(.ParameterKey == "EnvSuffix")) | .[0].ParameterValue ')
 
-echo "...Deploying SAM App ..."
-sam deploy
-if [ ${samStatusCode} -eq 0 ]; then 
-    echo "Done"
-else 
-    response=$?
-    exit ${response}
+echo "...Checking Enviroment Config to Deploy SAM App..."
+
+if [[ ! -z "$EnvSuffix" ]]; then 
+    sam deploy --config-env QA --s3-bucket ${SAMs3bucket} --kms-key-id ${SAMkmsKeyID}
+else sam deploy --s3-bucket ${SAMs3bucket} --kms-key-id ${SAMkmsKeyID}
 fi
+
+echo "...Done..."
