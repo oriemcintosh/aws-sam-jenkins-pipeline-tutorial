44ae1b0dcb050b7bffdd70f55ac546d704f00d87
Orie McIntosh <orie.mcintosh@slalom.com>
2023-01-08 20:53:25 -0500

    updated deploy script for testing against different parameter

--- config/sam-deploy.sh
+++ config/sam-deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# command to gather parameter value from AWS and assign to $EnvSuffix variable
-EnvSuffix=$(aws cloudformation describe-stacks --stack-name recovery-plan-orchestration |jq '.Stacks[].Parameters | map(select(.ParameterKey == "EnvSuffix")) | .[0].ParameterValue ')
+# command to gather parameter value from AWS and assign to $ConfigEnv variable
+ConfigEnv=$(aws cloudformation describe-stacks --stack-name recovery-plan-orchestration |jq '.Stacks[].Parameters | map(select(.ParameterKey == "ConfigEnv")) | .[0].ParameterValue ')
 
 echo "...Checking Enviroment Config to Deploy SAM App..."
 
-if [[ ! -z "$EnvSuffix" ]]; then 
-    sam deploy --config-env QA --s3-bucket ${SAMs3bucket} --kms-key-id ${SAMkmsKeyID}
+if [[ ! -z "$ConfigEnv" ]]; then 
+    sam deploy --config-env ${ConfigEnv} --s3-bucket ${SAMs3bucket} --kms-key-id ${SAMkmsKeyID}
 else sam deploy --s3-bucket ${SAMs3bucket} --kms-key-id ${SAMkmsKeyID}
 fi
 
